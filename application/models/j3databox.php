fb7a1d180add4d286f37a4213bfd05b1fb670b4b
MarchioeL <visarut.p@rtarf.mi.th>
2015-12-15 10:06:00 +0700

    no message

--- application/models/j3databox.php
+++ application/models/j3databox.php
@@ -41,12 +41,7 @@ Class j3databox extends CI_Model
 //------loading scrollbar
 
      
-	 function getEmployee(){	
-		$this->db->select("EMPLOYEE_ID,EMPLOYEE_NAME");
-		$this->db->from('trn_employee');				
-		$query = $this->db->get();		
-		return $query->result();			
-	}
+
 	
      function get_num_index(){
                 $this->db->select('*')->select('databox_upload.group_Id')->from('databox_upload')
@@ -314,11 +309,22 @@ function get_data_division()
 	 	  function get_data_type_up() 
     {
 	   
-		   $this->db->select('*')->select('data_group.dataId')->from('data_group')
-          ->join('data_type', 'data_type.type_id = data_group.dataId', 'LEFT');
-		   $this->db->group_by("dataId"); 
-	      $query = $this->db->get();
-		  return $query->result_array();	
+		   //$this->db->select('*')->select('data_group.dataId')->from('data_group8')
+                                                     
+		   
+
+	$this->db->select("*");
+		$this->db->from('data_type','data_group');		
+		  $this->db->where('data_type.type_id= data_group.dataId');
+		$query = $this->db->get();		
+		return $query->result();	
+
+		 //  SELECT * FROM `data_type`,`data_group` WHERE `data_type`.`type_id`= `data_group`.`dataId` GROUP BY `dataId`
+          
+		 //  ->join('data_type', 'data_type.type_id = data_group.dataId', 'LEFT');
+		 //  $this->db->group_by("dataId"); 
+	     // $query = $this->db->get();
+		 // return $query->result_array();	
     }
 
 //----------------------------------------- หน้าแสดงข้อมูลหลังจากค้นหา
--- application/views/databox_search.php
+++ application/views/databox_search.php
@@ -3,7 +3,7 @@
 										<?php $this->load->view('java/javascript_head.php'); ?>
 
 				 <input type  = "hidden" id="group_Id"         name="group_Id"      <?php echo "value=".$group_Id_f; ?>></input>
-				  <input type  = "hidden" id="divis_id"         name="divis_id"      <?php echo "value=".$divis_id_f; ?>></input>
+				  
                   <input type  = "hidden" id="data_group"       name="data_group"      ></input>
 				  <?php $this->load->view('java/javascript_box'); ?>
 <div class="span2">
@@ -16,7 +16,7 @@
                    	
           	 <div id="Person-1" class="box">
                  <div class="box-header">
-                            <i class="icon-user icon-large"></i>
+                            <i class="icon-user iconิ-large"></i>
                              <h5>ข้อมูล Databox</h5>
                         </div>
 						   <div class="box-content box-table">
@@ -24,9 +24,9 @@
                     <table id="tableId" class="table table-hover tablesorter">
                             <thead>
                                 <tr>
-                                    <th>เวลา</th>
-									<th>หมวดงาน</th>
-                                    <th>ชื่องาน</th>
+                                    <th>วันที่</th>
+									
+                                    <th>เรื่อง</th>
 							
                                 </tr>
                             </thead>
@@ -40,7 +40,6 @@
 						$hidden=$row['databox_id'];
 						echo "<tr>";
 						echo "<td onclick=\"chk(".$hidden.")\"  width='20%'>".$row['date_upload']."</a></td>";	
-						echo "<td onclick=\"chk(".$hidden.")\" width='20%'>".$row['groupname']."</a></td>";	
 						echo "<td onclick=\"chk(".$hidden.")\"  width='100%'>".$row['subject']."</a></td>";
 						echo "</tr>";
 						} 
@@ -52,7 +51,6 @@
 						
 						echo "<tr>";
 						echo "<td onclick=\"chk(".$hidden.")\"  width='20%'>".$row['date_upload']."</a></td>";	
-						echo "<td onclick=\"chk(".$hidden.")\" width='20%'>".$row['groupname']."</a></td>";	
 						echo "<td onclick=\"chk(".$hidden.")\"  width='100%'>".$row['subject']."</a></td>";
 						echo "</tr>";
 							}
